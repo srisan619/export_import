2e3282681f78b52956a248ae1bcb45d360349c9a
srinivasan <srinivasan@rorouser-desktop.(none)>
2013-03-06 15:45:25 +0530

    Done export & import files

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  #validates :name, :date, :price, :presence => true
-
+validates :name, :date, :price, :presence => true
+attr_accessible :name, :date, :price
 #  def self.to_csv(options={})
 #    CSV.generate(options) do |csv|
 #        csv << column_names
@@ -10,13 +10,32 @@ class Product < ActiveRecord::Base
 #    end
 #  end
 
-  def self.import(file)    
+  def self.import(file)
+    
 #      CSV.foreach(file.path, :headers => true) do |row|
 #          Product.create! row.to_hash
-        CSV.parse(file, :headers=> false) do |row|      
-      product = Product.create!(:name=>row[0],:date=>row[1],:price=>row[2])
-#        product.save
+#     CSV.parse('pro.csv', :headers=> false) do |row|
+#      product = Product.create!(:name=>row[0],:date=>row[1],:price=>row[2])
+ #       product.save!
+
+      spreadsheet = open_spreadsheet(file)
+      header = spreadsheet.row(1)
+      (2..spreadsheet.last_row).each do |i|
+         row = Hash[[header, spreadsheet.row(i)].transpose]
+       product = find_by_id(row["id"])
+       if product.nil?
+      Product.create! row.to_hash.slice(*accessible_attributes)
       end
+   end
+ end
+
+  def self.open_spreadsheet(file)
+  case File.extname(file.original_filename)
+  when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
+  when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+  when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+  else raise "Unknown file type: #{file.original_filename}"
   end
+end
 
 end
